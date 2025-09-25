4b355209e7ca26b71f783865d1527e0df6c2b0fa
maksymhlushkodir <maksymhlushko725@gmail.com>
2025-09-25 13:28:03 +0300

    Well, all over again

--- java-learning/zero_basics/src/FirstMethods.java
+++
@@ -1,9 +0,0 @@
-public class FirstMethods {
-    public static void Who_is_greedy(String who){
-        System.out.println(who + " GREED!");
-    }
-    public static void main(String[] args) {
-        String who = "I am";
-        Who_is_greedy(who);
-    }
-}
